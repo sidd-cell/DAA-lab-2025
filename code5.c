c816c252f8c67f09a22d5b72ea29aa962dab1a6b
sidd-cell <siddchauhan59861@gmail.com>
2025-08-22 13:07:47 +0530

    Update code5.c

--- code5.c
+++ code5.c
@@ -2,7 +2,7 @@
 int main()
 {
     int i,n,a=0,b=1,c;
-    printf("give number upto which Fibonacci series is to be printed: ");
+    printf("give number of terms which should be there in Fibonacci series : ");
     scanf("%d", &n);
     printf("\t%d,%d", a,b);
     for(i=2; i<n; i++)
@@ -13,4 +13,5 @@ int main()
         b = c;
     }
     return 0;
+
 }
